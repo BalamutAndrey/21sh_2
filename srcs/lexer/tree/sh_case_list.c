3a54bcc5ceaa629ccc1087903a59b67ec83531d8
Earlean Boris <eboris@il-i3.msk.21-school.ru>
2020-09-05 17:08:32 +0300

    Tree: add case_list (no full)

---
+++ srcs/lexer/tree/sh_case_list.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_case_list.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eboris <eboris@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/05 17:06:31 by eboris            #+#    #+#             */
+/*   Updated: 2020/09/05 17:07:39 by eboris           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sh_main.h"
+
+/*
+** case_list        : case_list case_item
+**                  |           case_item
+*/
+
+t_node	*sh_caselist(t_main *main)
+{
+	t_node	*temp;
+
+	temp = NULL;
+	return (NULL);
+}
+
+/*
+** case_list        : case_list case_item
+*/
+
+t_node	*sh_caselist_caselist_caseitem(t_main *main)
+{
+	t_node	*temp;
+
+	temp = NULL;
+	return (NULL);
+}
+
+/*
+** case_list        :           case_item
+*/
+
+t_node	*sh_caselist(t_main *main)
+{
+	t_node	*temp;
+
+	temp = NULL;
+	return (NULL);
+}
