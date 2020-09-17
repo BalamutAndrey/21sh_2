521ae8a4cb728cb57a62a4d1f5bb56d4526d1ed3
Gall Eliz <geliz@il-c2.msk.21-school.ru>
2020-09-17 18:22:51 +0300

    lexer.h

--- includes/sh_lexer.h
+++ includes/sh_lexer.h
@@ -3,72 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   sh_lexer.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eboris <eboris@student.21-school.ru>       +#+  +:+       +#+        */
+/*   By: geliz <geliz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/03 16:38:41 by eboris            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/09/15 16:06:23 by eboris           ###   ########.fr       */
-=======
-/*   Updated: 2020/09/17 18:10:58 by geliz            ###   ########.fr       */
->>>>>>> 80722f1fc9a0992835af7c9b6dd39e44953d98ff
+/*   Updated: 2020/09/17 18:22:39 by geliz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SH_LEXER_H
 # define SH_LEXER_H
 
-<<<<<<< HEAD
-typedef enum    e_type {
-    NONE,
-    COMPLETE_COMMAND,
-    LIST,
-    PIPELINE,
-    AND_OR,
-    COMMAND,
-    CMDNAME,
-    CMDSUFFIX,
-    SEPARATOR,
-
-    WORD,
-    NEWLINE,
-	IO_NUMBER,
-	AND_IF,			// &&
-	OR_IF,			// ||
-	DSEMI,			// ;;
-	DLESS,			// <<
-	DGREAT,			// >>
-	LESSAND,		// <&
-	GREATAND,		// >&
-	LESSGREAT,		// <>
-	DLESSDASH,		// <<-
-	CLOBBER,		// >|
-	LBRACE,			// {
-	RBRACE,			// }
-	BANG			// !
-}               t_type;
-
-
-//
-//      TOKEN
-//
-typedef struct      s_token {
-    t_type          type;
-    char            *content;
-    struct s_token	*next;
-}                   t_token;
-
-//
-//      AST_TREE
-//
-typedef struct      s_node {
-//    t_type          node_type;
-    t_token         *token;
-    struct s_node   *left;
-    struct s_node   *right;
-}                   t_node;
-
-=======
->>>>>>> 80722f1fc9a0992835af7c9b6dd39e44953d98ff
 /*
 ** sh_lexer.c
 */
