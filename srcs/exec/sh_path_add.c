a78e98c0df6d646516f05e5bfc83e94407c86c41
Earlean Boris <eboris@il-i3.msk.21-school.ru>
2020-11-01 17:18:11 +0300

    path_add check \0

--- srcs/exec/sh_path_add.c
+++ srcs/exec/sh_path_add.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sh_path_add.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: geliz <geliz@student.42.fr>                +#+  +:+       +#+        */
+/*   By: eboris <eboris@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 16:53:48 by eboris            #+#    #+#             */
-/*   Updated: 2020/10/31 17:45:04 by geliz            ###   ########.fr       */
+/*   Updated: 2020/11/01 17:16:54 by eboris           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 void	sh_path_add(t_main *main, t_exec *temp)
 {
-	if (temp->argv[0][0] == '.')
-		sh_run_write_string_point(main, temp);
-	else if (temp->argv[0][0] == '~')
-		sh_run_write_string_home(main, temp);
-	else if ((temp->argv[0][0] != '/') &&
-		(sh_is_builtin(temp->argv[0]) == false))
-		sh_find_path(main, temp);
+	if (temp->argv != NULL && temp->argv[0][0] != '\0')
+	{
+		if (temp->argv[0][0] == '.')
+			sh_run_write_string_point(main, temp);
+		else if (temp->argv[0][0] == '~')
+			sh_run_write_string_home(main, temp);
+		else if ((temp->argv[0][0] != '/') &&
+			(sh_is_builtin(temp->argv[0]) == false))
+			sh_find_path(main, temp);
+	}
 //	ft_printf("\ncmd = %s\n", temp->cmd_arg[0]);
 }
 
